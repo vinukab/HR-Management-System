9ea0d5147ee7c93a896e0a796fc85c0d179a47b7
SahanJDesigns <161017879+SahanJDesigns@users.noreply.github.com>
2024-10-26 01:02:51 +0530

    Delete app directory

--- app/(Routes)/leave/page.js
+++
@@ -1,7 +0,0 @@
-const leave = () => {
-    return ( 
-        <h1>leave page</h1>
-     );
-}
- 
-export default leave;
--- app/(Routes)/settings/page.js
+++
@@ -1,9 +0,0 @@
-const settings = () => {
-    return ( 
-        <div>
-            <h1>Settings Page</h1>
-        </div>    
-     );
-}
- 
-export default settings;
